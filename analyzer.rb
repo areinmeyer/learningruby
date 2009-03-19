aff1703fcf1c17d851ef9eaca4bcb1fcdf10c0f6
Allen Reinmeyer <allen.reinmeyer@gmail.com>
2009-03-19 07:20:24 -0400

    analyzer converted to class, added basic logging

--- analyzer.rb
+++ analyzer.rb
@@ -13,33 +13,56 @@
 
 =end
 
+require 'logger'
+
 # Analyze the file for statistics
-def analyze_file(file)
-  empty_line = false
-  stats  = Hash.new() {|k,v| k[v] = 0}
-  file.each do |line|
-    stats[:line_count] += 1
-    stats[:char_count] += line.length
-    stats[:count_non_space] += line.length - line.count(" ")
-    stats[:word_count] += line.split(' ').length
-    stats[:sentence_count] += line.scan(/[.!?]{1}/).length
-    
-    #prevent double empty lines from being counted.
-    if (line.strip == '')
-      stats[:paragraph_count] += 1 unless empty_line
-      empty_line = true
-    else
-      empty_line = false
-    end
+class TextFileAnalyzer
+  def initialize(file)
+    @file = file
+    @log = Logger.new("analyzer.log")
+    @stats  = Hash.new() {|k,v| k[v] = 0}
+    @log.level = Logger::DEBUG
   end
   
-  # Last paragraph not counted if no blank line after it
-  stats[:paragraph_count] += 1 unless empty_line
-  return stats
+  def analyze_file
+    empty_line = false
+    file = File.open(@file, 'r+')
+    
+    @log.debug("Analyzing File... #{file.path}")
+    file.each do |line|
+      @stats[:line_count] += 1
+      @stats[:char_count] += line.length
+      @stats[:count_non_space] += line.length - line.count(" ")
+      @stats[:word_count] += line.split(' ').length
+      @stats[:sentence_count] += line.scan(/[.!?]{1}/).length
+      
+      #prevent double empty lines from being counted.
+      if (line.strip == '')
+        @log.debug("Encountered empty line...")
+        @stats[:paragraph_count] += 1 unless empty_line
+        empty_line = true
+      else
+        empty_line = false
+      end
+    end
+    
+    # Last paragraph not counted if no blank line after it
+    @stats[:paragraph_count] += 1 unless empty_line
+    
+    log_stats
+    return @stats
+  end
+  
+  private
+  
+  def log_stats
+    @stats.each {|k,v| @log.info "#{k.id2name} is #{@stats[k]}"}
+  end
 end
 
-file = File.open("text.txt", 'r+')
-stats = analyze_file(file)
+
+analyzer = TextFileAnalyzer.new("text.txt")
+stats = analyzer.analyze_file
 
 stats.each {|k,v| puts "#{k.id2name} is #{stats[k]}"}
 
